1586dd0bb3b54f9e9c0fe307e7752bc420711290
CUB3D <callumthom11@gmail.com>
2020-03-30 02:24:06 +0100

    format and remove unstable feature

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,12 @@
-#![feature(async_closure)]
-
-use actix_web::{HttpServer, App, Error};
 use actix_files::{Files, NamedFile};
-use actix_web::middleware::{Logger, Compress, NormalizePath};
-
+use actix_web::middleware::{Compress, Logger, NormalizePath};
+use actix_web::{App, Error, HttpServer};
 
 use actix_web::dev::{ServiceRequest, ServiceResponse};
-use std::io::Read;
 use actix_web::http::{HeaderName, HeaderValue};
+use std::io::Read;
 
-async fn fallback(
-    req: ServiceRequest
-) -> Result<ServiceResponse, Error> {
+async fn fallback(req: ServiceRequest) -> Result<ServiceResponse, Error> {
     // Not possible to pass via actix data so retrive the base path again
     let args: Vec<_> = std::env::args().collect();
     let root_dir = args.get(1).unwrap_or(&".".to_string()).clone();
@@ -20,17 +15,21 @@ async fn fallback(
     let path = req.path().replacen("/", "", 1);
     let fullpath = format!("{}/{}.html", root_dir, path);
 
-    let data = NamedFile::open(&fullpath).
-        map(| mut x | {
+    let data = NamedFile::open(&fullpath)
+        .map(|mut x| {
             let mut str = String::new();
-            x.read_to_string(&mut str).expect(format!("Failed to read file {}", fullpath).as_str());
+            x.read_to_string(&mut str)
+                .expect(format!("Failed to read file {}", fullpath).as_str());
             str
         })
         .unwrap_or("<body><h1>404 file not found</h1></body>".to_string());
 
     let mut res = req.into_response(data);
     res.headers_mut().remove("content-type");
-    res.headers_mut().append(HeaderName::from_static("content-type"), HeaderValue::from_static("text/html"));
+    res.headers_mut().append(
+        HeaderName::from_static("content-type"),
+        HeaderValue::from_static("text/html"),
+    );
     Ok(res)
 }
 
@@ -42,14 +41,14 @@ async fn main() -> std::io::Result<()> {
     println!("Serving {} on 0.0.0.0:8080", root_dir);
 
     HttpServer::new(move || {
-
         App::new()
-            .service(Files::new("/", &root_dir)
-                .index_file("index.html")
-                .show_files_listing()
-                .redirect_to_slash_directory()
-                .use_etag(true)
-                .default_handler(fallback)
+            .service(
+                Files::new("/", &root_dir)
+                    .index_file("index.html")
+                    .show_files_listing()
+                    .redirect_to_slash_directory()
+                    .use_etag(true)
+                    .default_handler(fallback),
             )
             .wrap(Logger::default())
             .wrap(Compress::default())
